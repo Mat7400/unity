7c73a3720d7adf717484019817b72ce08339b553
Mat7400 <mmatuha74@gmail.com>
2022-02-01 20:05:47 +0300

    0102
    0102

---
+++ Assets/Buttyon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Buttyon : MonoBehaviour
+{
+    Text classtext;
+    // Start is called before the first frame update
+    public void setPlayerName()
+    {
+        string pname = "test";
+        //get text element in Unity
+        classtext = GameObject.Find("TextPlayerName").GetComponent<Text>();
+        classtext.text = pname;
+        classtext.color = Color.red;
+    }
+    public void Start()
+    {
+        Debug.Log("test");
+
+    }
+    public void Update()
+    {
+        Debug.Log("Update");
+    }
+}
---
+++ Assets/handler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class handler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
