dfce9c62edf791c49d5c9d50d3df04977caff16e
Mat7400 <mmatuha74@gmail.com>
2022-02-05 12:08:45 +0300

    0502

--- Assets/Buttyon.cs
+++ Assets/Buttyon.cs
@@ -8,11 +8,25 @@ public class Buttyon : MonoBehaviour
     // Start is called before the first frame update
     public void setPlayerName()
     {
-        string pname = "test";
+        //get input field
+        var input = GameObject.Find("InputField").GetComponent<InputField>();
+        string text = input.text;
         //get text element in Unity
-        classtext = GameObject.Find("TextPlayerName").GetComponent<Text>();
-        classtext.text = pname;
+        classtext = GameObject.Find("TextTextPlayer").GetComponent<Text>();
+        classtext.text = text;
         classtext.color = Color.red;
+        Debug.Log("tet setPlayerName");
+    }
+    public void menubutton()
+    {
+        var input = GameObject.Find("InputField");
+        input.SetActive(false);
+        //move
+        var cs = GameObject.Find("TextTextPlayer").GetComponent<RectTransform>();
+        var vect = cs.offsetMax;
+        vect.x = vect.x + 15;
+        vect.y = vect.y + 15;
+        cs.offsetMax = vect;
     }
     public void Start()
     {
