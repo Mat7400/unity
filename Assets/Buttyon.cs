84962f0344f00cda8d3dd3043e4317f17648bafc
Mat7400 <mmatuha74@gmail.com>
2022-02-06 15:04:51 +0300

    0602

--- Assets/Buttyon.cs
+++ Assets/Buttyon.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Buttyon : MonoBehaviour
 {
+    int counter = 0;
     Text classtext;
     // Start is called before the first frame update
     public void setPlayerName()
@@ -17,24 +18,46 @@ public class Buttyon : MonoBehaviour
         classtext.color = Color.red;
         Debug.Log("tet setPlayerName");
     }
-    public void menubutton()
+    
+        public void menubutton()
     {
         var input = GameObject.Find("InputField");
-        input.SetActive(false);
+        if (input!=null)
+            input.SetActive(false);
         //move
-        var cs = GameObject.Find("TextTextPlayer").GetComponent<RectTransform>();
-        var vect = cs.offsetMax;
-        vect.x = vect.x + 15;
-        vect.y = vect.y + 15;
-        cs.offsetMax = vect;
-    }
+        //var cs = GameObject.Find("TextTextPlayer").GetComponent<RectTransform>();
+        //var vect = cs.offsetMax;
+        //vect.x = vect.x + 15;
+        //vect.y = vect.y + 15;
+        //cs.offsetMax = vect;
+
+        //move karych
+        var karychO = GameObject.Find("KARYCH");
+        var transform = karychO.GetComponent<Transform>();
+        //position - двигает в координаты вектора
+        //transform.position += new Vector3(30, 15, 0);
+        //КАРЫЧА ДВИГАТЬ НА ЧУТЬ_ЧУТЬ зависит от размера
+        transform.Translate(3, 3, 0);
+
+        var transform2 = GameObject.Find("TextTextPlayer").GetComponent<Transform>();
+        //transform2.position += new Vector3(30, 30, 0);
+        //translate - двигает из того места где уже был
+        transform2.Translate(40, 40, 0);
+        Debug.Log("BUTTON OK "+ counter);
+        counter++;
+    } 
     public void Start()
     {
         Debug.Log("test");
 
     }
+    float speed = 1;
     public void Update()
     {
+        var transform2 = GameObject.Find("TextTextPlayer").GetComponent<Transform>();
+        //transform2.position += new Vector3(30, 30, 0);
+        //translate - двигает из того места где уже был
+        transform2.Translate(Vector3.forward * speed * Time.deltaTime);
         Debug.Log("Update");
     }
 }
