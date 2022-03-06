ccbfe8ff7666306c26a2ff08effe979264026d70
Mat7400 <mmatuha74@gmail.com>
2022-03-06 14:53:17 +0300

    0603 animation

--- Assets/battlePlayer.cs
+++ Assets/battlePlayer.cs
@@ -25,7 +25,7 @@ public class Player
         System.Random rondom = rnd;
         minDamage = rondom.Next(1, 5);
         maxDamage = rondom.Next(8, 12);
-        HealPoints = rondom.Next(10, 25);
+        HealPoints = rondom.Next(50, 100);
     }
     public string gid { get; set; }
     public int maxDamage = 10;
@@ -126,34 +126,59 @@ public class battlePlayer : MonoBehaviour
         //1)get sprite from assets
         //2)set sprite to playerObj
         int ava = rnd.Next(2, 6);
-        var sp = Resources.Load("ava"+ava.ToString().Trim()) as Sprite;
-        var render = playerObj.GetComponent<SpriteRenderer>();
-        render.sprite = sp;
-        //if (ava==2)
-        //{
-        //    var sp = Resources.Load("ava2") as Sprite;
-        //    var render = playerObj.GetComponent<SpriteRenderer>();
-        //    render.sprite = sp;
-        //}
-        //else if (ava==3)
-        //{
-        //    var sp = Resources.Load("ava3") as Sprite;
-        //    var render = playerObj.GetComponent<SpriteRenderer>();
-        //    render.sprite = sp;
-        //}
-        //else if (ava == 4)
-        //{ }
-        //else if (ava == 5)
-        //{ }
-        //else if (ava == 6)
-        //{ }
+        // Assets/ava4.png 
+        //Assets / ava3.jpg
+        //Assets/ava2.jpg
+        //Assets / ava3.jpg
+        //Assets / ava5.png
+        //Assets/ava6.png
+
+        if (ava == 2)
+        {
+            //Assets / Resources / ava2.jpg
+            loadSprite("ava2", playerObj);
+        }
+        else if (ava == 3)
+        {
+            //Assets/Resources/ava2.jpg
+            loadSprite("ava3", playerObj);
+        }
+        else if (ava == 4)
+        {
+            loadSprite("ava4", playerObj);
+        }
+        else if (ava == 5)
+        {
+            loadSprite("ava5", playerObj);
+        }
+        else if (ava == 6)
+        {
+            loadSprite("ava6", playerObj);
+        }
+    }
+    public void loadSprite(string name, GameObject playerObj)
+    {
+        //06.03 ошибка загрузки спрайтов в этой строке
+        //нужно создать папку Resources
+        //не помогло
+        var sp = Resources.Load(name) as Sprite;
+        if (sp != null)
+        {
+            var render = playerObj.GetComponent<SpriteRenderer>();
+            render.sprite = sp;
+
+        }
+        else
+        {
+            Debug.Log("NO SPRITE "+ name);
+        }
     }
     // Update is called once per frame
     void Update()
     {
         //animate player and enemy
         //60fps - once in 1s
-        if (count % 60 == 0 && gameend==false)
+        if (count % 120 == 0 && gameend==false)
         {
             var playerObj = GameObject.Find("player2");
             var enemyObj = GameObject.Find("enemy");
@@ -174,60 +199,72 @@ public class battlePlayer : MonoBehaviour
             if (xenemy >= rightborder || xenemy <= lefrborder)
                 //change speed
                 enemyspeed = enemyspeed * (-1);
-            if (xplayer >= lefrborder || xplayer <= lefrborder)
+            //06.03 было неверное условие!
+            if (xplayer <= lefrborder || xplayer >= rightborder)
                 //change speed
                 playerspeed = playerspeed * (-1);
 
-
+            var classtext = GameObject.Find("TextDamage").GetComponent<Text>();
+            classtext.text = player.name + " HP=" + player.HealPoints + " -- " + enemy.name + " HP=" + enemy.HealPoints + " Xp=" + xplayer + " Xe=" + xenemy;
             //if collide - count damage, change speed
-            //
-            if ( (xplayer - xenemy < 6) || xplayer > xenemy)
+            //06.03 тщательнее прописать условие столкновения!
+            
+            if (xplayer >= xenemy)
             {
-                //change speed
-                playerspeed = playerspeed * (-1);
-                enemyspeed = enemyspeed * (-1);
-                //change x coord
-                xplayer = xplayer + playerspeed;
-                xenemy = xenemy + enemyspeed;
-
-                //random damage
-                var dmage1 = player.CountDamage();
-                var dmg2 = enemy.CountDamage();
-                player.dealdamage(dmg2);
-                enemy.dealdamage(dmage1);
-                var classtext = GameObject.Find("TextDamage").GetComponent<Text>();
-                if (player.isAlive() == false && enemy.isAlive() == false)
+                if (playerspeed > 0 && enemyspeed < 0)
                 {
-                    //listbox - массив dealedDamage вывести
+                    //change speed
+                    playerspeed = playerspeed * (-1);
+                    enemyspeed = enemyspeed * (-1);
+                    //change x coord
+                    xplayer = xplayer + playerspeed;
+                    xenemy = xenemy + enemyspeed;
 
-                    classtext.text = "TIE play";
-                    //stop update
-                    gameend = true;
-                }
-                else
-                {
-                    if (player.isAlive() == false)
+                    //random damage
+                    var dmage1 = player.CountDamage();
+                    var dmg2 = enemy.CountDamage();
+                    player.dealdamage(dmg2);
+                    enemy.dealdamage(dmage1);
+                    
+                    if (player.isAlive() == false && enemy.isAlive() == false)
                     {
+                        //listbox - массив dealedDamage вывести
 
-                        classtext.text = "player LOSE";
+                        classtext.text = "TIE play";
                         //stop update
                         gameend = true;
-
-                    }
-                    else if (enemy.isAlive() == false)
-                    {
-
-                        classtext.text = "player WIN";
-                        //stop update
-                        gameend = true;
-
                     }
                     else
                     {
-                        //show damage on TextDamage text field
+                        if (player.isAlive() == false)
+                        {
 
-                        classtext.text = player.name + " HP=" + player.HealPoints + " -- " + enemy.name + " HP=" + enemy.HealPoints;
+                            classtext.text = "player LOSE";
+                            //stop update
+                            gameend = true;
+
+                        }
+                        else if (enemy.isAlive() == false)
+                        {
+
+                            classtext.text = "player WIN";
+                            //stop update
+                            gameend = true;
+
+                        }
+                        else
+                        {
+                            //show damage HP on TextDamage text field
+
+                            
+                        }
                     }
+                }
+                else 
+                {
+                    //change x coord
+                    xplayer = xplayer + playerspeed;
+                    xenemy = xenemy + enemyspeed;
                 }
             }
             
